affd7064e55cfbc4d933628513fc8dec6123d5da
landa <landan2001@mail.ru>
2025-02-26 21:53:34 +0400

    fix

--- test_db.py
+++ test_db.py
@@ -2,7 +2,7 @@
 from board.models import TimeLog
 
 # Получаем запись TimeLog по конкретному ID
-time_log_entry = TimeLog.objects.get(uuid="fe29bd76fa0d432bbaaa0d07ba3e6c55")  # Замените 1 на нужный ID записи
+time_log_entry = TimeLog.objects.get(uuid="fe29bd76fa0d432bbaaa0d07ba3e6c55")
 
 # Удаляем запись из базы данных
 time_log_entry.delete()
